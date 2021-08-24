38b2f225452f6d3c0aa6cd4a4a3622eedc6d969a
Julio Cesar <juliocsrmf@gmail.com>
2021-08-24 19:10:13 -0300

    Pegnado uma ou mais frase

--- src/controllers/api.controller.ts
+++ src/controllers/api.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 
-import { Phrase } from '../models/Phrase';
+import { Phrase, PhraseInstance } from '../models/Phrase';
 
 export const ping = (req: Request, res: Response) => {
     res.json({ pong: true });
@@ -21,5 +21,28 @@ export const createPhrase = async (req: Request, res: Response) => {
 
     let newPhrase = await Phrase.create({ author, txt });
 
+    res.status(201); // Created
     res.json({ id: newPhrase.id, author, txt });
+}
+
+export const getAllPhrases = async (req: Request, res: Response) => {
+    let phrases = await Phrase.findAll();
+
+    res.status(200);
+    res.json({ phrases });
+}
+
+export const getPhrase = async (req: Request, res: Response) => {
+    let phrase: PhraseInstance | null = null;
+    
+    if(req.params.id && !isNaN(parseInt(req.params.id as string))) {
+        phrase = await Phrase.findByPk(req.params.id);
+    }
+
+    if (phrase) {
+        res.json({ phrase });
+    } else {
+        res.status(404);
+        res.json({ error: 'Frase Não encontrada' })
+    }
 }
