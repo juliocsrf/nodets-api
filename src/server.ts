9f2e0beafb2405c7c92f52d3a7cdba71dd725ec1
Julio Cesar <juliocsrmf@gmail.com>
2021-08-24 15:44:59 -0300

    Criando API simples

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
+import apiRoutes from './routes/api';
 
 console.log('Server starting...');
 dotenv.config();
@@ -9,6 +10,8 @@ const server = express();
 
 server.use(express.static(path.join(__dirname, '../public')));
 server.use(express.urlencoded({ extended: true }));
+
+server.use('/api', apiRoutes);
 
 server.use((req: Request, res: Response) => {
     res.status(404);
